9e52ad63af0d7c619096f9065b1f3c6355aacc49
Shailla <107023318+shai-mohan@users.noreply.github.com>
2026-01-22 15:45:44 +0800

    fix vite file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/paws-preferences/",
+  base: "/paws-app/",
   plugins: [react()],
 });
 
